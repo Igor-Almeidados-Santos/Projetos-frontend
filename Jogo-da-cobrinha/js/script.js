0e42a84a7f890b28fc663a99fdd50a6114f52c83
IGOR ALMEIDA DOS SANTOS <75753575+Igor-Almeidados-Santos@users.noreply.github.com>
2021-06-06 15:51:09 -0300

    Jogo da cobrinha

--- Jogo-da-cobrinha/js/script.js
+++ Jogo-da-cobrinha/js/script.js
@@ -1,24 +1,32 @@
 
-function criarBG(){
+function createBG(){
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
 };
 
-function criarCobrinha(){
+function createSnake(){
     for(let i =0; i < snake.length; i++){
         context.fillStyle = 'green';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     };
 };
 
-function iniciarJogo(){
+function startGame(){
     if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
-    if(snake[0].x < 10 && direction === 'left') snake[0].x = 16 * box;
+    if(snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
-    if(snake[0].y < 10 && direction === 'up') snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
 
-    criarBG();
-    criarCobrinha();
+    for(let i = 1; i < snake.length; i++){
+        if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+            clearInterval(game);
+            alert(`Game over! :(`);
+        }
+    }
+
+    createBG();
+    createSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -28,7 +36,12 @@ function iniciarJogo(){
     if(direction === 'up') snakeY -= box;
     if(direction === 'down') snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+       food.x = Math.floor(Math.random() * 15 + 1) * box;
+       food.y = Math.floor(Math.random() * 15 + 1) * box;
+    };
 
     let newHead = {
         x: snakeX,
@@ -45,6 +58,12 @@ function update(event){
     if(event.keyCode === 40 && direction != 'up') direction = 'down';
     
 }
+function drawFood(){
+    context.fillStyle = 'red';
+    context.fillRect(food.x, food.y, box, box);
+}
+
+
 
 document.addEventListener('keydown', update);
 
@@ -56,6 +75,9 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 };
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+};
 let direction = 'right';
-let jogo = setInterval(iniciarJogo, 100);
+let game = setInterval(startGame, 100);
